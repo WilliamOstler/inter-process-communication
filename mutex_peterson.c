3f0df2382c542845a7b58b6c2c2cf3f23b46e6ac
williamostler <wostler@live.co.uk>
2022-03-21 11:38:44 +0000

    Implemented mutex_enter in mutex_peterson.c

--- mutex_peterson.c
+++ mutex_peterson.c
@@ -5,17 +5,6 @@
 #include "mutex_peterson.h"
 #include <errno.h>
 
-/* mutex.h contains the specification of functions in this file */
-
-/* 
- * TODO: you must implement this function
- * Hints:
- * - see mutex_lockvar.c for how mutex_new is implemented there but remember
- *      that the underlying mutex type allocated in shared memory and 
- *      encapsulated by the IPC type will be different
- * - the shared object label should be mux_peters
- * - also see: mutex_peterson.h, mutex_lockvar.c and ipc.h
- */
 mutex_t* mutex_new(proc_t* proc) {
 
     if(!proc){
@@ -25,19 +14,17 @@ mutex_t* mutex_new(proc_t* proc) {
     return ipc_new(proc, "mux_peters", sizeof(mutex_t));
 }
 
-/* 
- * TODO: you must implement this function.
- * Hints:
- * - see the section on the Peterson's busy waiting solution in 
- *     the CSC2035 lecture material on IPC
- * - process ids must be normalised to either a 0 or 1 for indexing 
- *      into the interested array (there is an arithmetic operator to do this)
- * - remember a mutex_t object is basically an ipc object and the specific
- *      implementation of a mutex (Peterson's in this case) in
- *      shared memory is at the addr field (see ipc.h)
- * - also see: mutex_lockvar.c
- */
 void mutex_enter(mutex_t* mux) {
+    if(mux){
+        mutex_peterson_t* mp = (mutex_peterson_t*) mux->addr;
+
+        int other_id = 1 - mux->proc->id;
+        mp->interested[mux->proc->id % 2] = 1;
+        mp->turn = mux->proc->id;
+
+        while(mp->turn == mux->proc->id && mp->interested[other_id] == 1){
+        }
+    }
     return;
 }
 
